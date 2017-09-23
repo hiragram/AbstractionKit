06a7a9abaeb62e3a0e02429581d18b449110b368
Yuya Hirayama <y@yura.me>
2017-09-24 00:03:32 +0900

    Add Xcode project

---
+++ AbstractionKit/AbstractionKit.h
@@ -0,0 +1,19 @@
+//
+//  AbstractionKit.h
+//  AbstractionKit
+//
+//  Created by Yuya Hirayama on 2017/09/24.
+//  Copyright © 2017年 Yuya Hirayama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AbstractionKit.
+FOUNDATION_EXPORT double AbstractionKitVersionNumber;
+
+//! Project version string for AbstractionKit.
+FOUNDATION_EXPORT const unsigned char AbstractionKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AbstractionKit/PublicHeader.h>
+
+
